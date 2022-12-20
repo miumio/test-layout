f198825f826dcbb3604d74abeee8ecbbaa4ca0e6
Asya Nikolskaya <nickolskaya@gmail.com>
2022-12-20 21:02:25 +0300

    refactor: menu

--- source/js/modules/init-menu.js
+++ source/js/modules/init-menu.js
@@ -1,35 +1,30 @@
-import { ScrollLock } from '../utils/scroll-lock';
-const breakpointMd = window.matchMedia('(max-width:1023px)');
-
 export const initMenu = () => {
     const header = document.querySelector('.page-header');
-    window.scrollLock = new ScrollLock();
 
-    if (header) {
-        const TOGGLER_SELECTOR = '.burger';
-        const toggler = header.querySelector(TOGGLER_SELECTOR);
-        const menu = header.querySelector('.main-nav');
-
-        const toggleMenu = () => {
-            toggler.classList.toggle('on');
-            menu.classList.toggle('is-open');
-            const isActive = header.classList.toggle('nav-is-open');
-            toggler.setAttribute(
-                'aria-label',
-                toggler.dataset[isActive ? 'opened' : 'closed']
-            );
-        };
-
-        header.addEventListener('click', (ev) => {
-            if (ev.target.closest(TOGGLER_SELECTOR)) {
-                toggleMenu();
-            }
-
-            if (menu.classList.contains('is-open') && breakpointMd.matches) {
-                window.scrollLock.disableScrolling();
-            } else {
-                window.scrollLock.enableScrolling();
-            }
-        });
+    if (!header) {
+        return;
     }
+
+    function toggleMenu() {
+        const menu = header.querySelector('.main-nav');
+        menu.classList.toggle('is-open');
+    }
+
+    function toggleBtn() {
+        const btn = header.querySelector('.burger');
+        const isActive = header.classList.toggle('nav-is-open');
+
+        btn.classList.toggle('on');
+        btn.setAttribute(
+            'aria-label',
+            btn.dataset[isActive ? 'opened' : 'closed']
+        );
+    }
+
+    header.addEventListener('click', (ev) => {
+        if (ev.target.closest('.burger')) {
+            toggleMenu();
+            toggleBtn();
+        }
+    });
 };
